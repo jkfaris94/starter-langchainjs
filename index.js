efd962846938a3b4a5b5f92705becbaa14b0c78d
Johnny Faris <jkfaris@gmail.com>
2025-06-16 20:04:55 -0700

    followed lesson instructions. working properly

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 import { ChatCohere } from "@langchain/cohere";
-import dotenv from 'dotenv';
+import { PromptTemplate } from "@langchain/core/prompts";
+import { RunnableSequence } from "@langchain/core/runnables";
+import { StringOutputParser } from "@langchain/core/output_parsers";
+
+import dotenv from "dotenv";
 
 dotenv.config();
 
-const model = new ChatCohere({apiKey: process.env.COHERE_API_KEY});
+const model = new ChatCohere({ apiKey: process.env.COHERE_API_KEY });
+
+const promptTemplate = PromptTemplate.fromTemplate("Give me a recipe for {food}")
+
+const outputParser = new StringOutputParser();
+
+const chain = RunnableSequence.from([promptTemplate, model, outputParser])
+
+const result = await chain.invoke({food: "apple pie"});
+
+console.log(result);
